8f37f5b2926a86c1ced84488b47bf77458b87908
Panchis7u7 <smadrigal.rod@gmail.com>
2020-12-21 09:56:29 -0600

    Controlador de servos.

--- Pruebas/Top.v
+++ Pruebas/Top.v
@@ -1,5 +1,11 @@
-module Top (x1, x2, x3, z1, z2);
-input x1, x2, x3;
-output z1, z2;
+module Top (
+	input x1,
+	input x2,
+	input x3,
+	output wire z1
+);
 
+nand inst1(net1, x1, x2, x3);
+nand inst2(net2, x1, x2, x3);
+and inst3(z1, net1, net2);
 endmodule
